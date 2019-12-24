06843ec82dafcad9989db32a03349cda067b5b7c
Spetrut <petrutstoica123@yahoo.com>
2019-12-24 14:36:29 +0200

    Create colors, mixins, spacing and typography files

---
+++ src/styles/colors.js
@@ -0,0 +1,7 @@
+export const PRIMARY_1 = '#f54b64';
+export const PRIMARY_2 = '#f78361';
+export const SECONDARY = '#242A38';
+export const WHITE = '#FFFFFF';
+export const BLACK = '#000000';
+export const DARK_GREY='#4E586E';
+
